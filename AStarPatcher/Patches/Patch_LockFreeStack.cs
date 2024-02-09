3a3e7e48e9e7310ac552a6b44b22c701e0809428
Leonid <vama.ru472159472159@gmail.com>
2024-02-10 02:41:41 +0700

    Return patch of LockFreeStack

---
+++ AStarPatcher/Patches/Patch_LockFreeStack.cs
@@ -0,0 +1,37 @@
+﻿using HarmonyLib;
+using Pathfinding;
+using Pathfinding.Util;
+
+namespace AStarPatcher.Patches;
+
+[HarmonyPatch(typeof(LockFreeStack))]
+internal class Patch_LockFreeStack
+{
+    private static readonly object s_Lock = new();
+
+    [HarmonyPatch(nameof(LockFreeStack.Push))]
+    [HarmonyPrefix]
+    public static bool PushPatch(LockFreeStack __instance, Path p)
+    {
+        lock (s_Lock)
+        {
+            p.next = __instance.head;
+            __instance.head = p;
+
+            return false;
+        }
+    }
+
+    [HarmonyPatch(nameof(LockFreeStack.PopAll))]
+    [HarmonyPrefix]
+    public static bool PopAllPatch(LockFreeStack __instance, out Path __result)
+    {
+        lock (s_Lock)
+        {
+            __result = __instance.head;
+            __instance.head = null;
+
+            return false;
+        }
+    }
+}
